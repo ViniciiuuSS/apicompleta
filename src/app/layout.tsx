675584b3eb8ba75d73c4f01a38e71582207798d9
vinicius guedes <vinicraft556@gmail.com>
2025-04-15 22:48:32 -0300

    Fix - Remo default dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="pt-br">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body>
         <AuthProvider>
