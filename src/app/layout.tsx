9c2d39bcf9db084c0482fe4b2406cdd5dec7991b
vinicius guedes <vinicraft556@gmail.com>
2025-04-15 22:47:49 -0300

    Mode default dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="dark">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body>
         <AuthProvider>
